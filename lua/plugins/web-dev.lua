20acc1788380ff88ebfb14f7072ec616daae7daa
James Glover <james.glover7@outlook.com>
2026-01-12 21:39:40 +0000

    Changed to vscode typescript lsp

--- lua/plugins/web-dev.lua
+++ lua/plugins/web-dev.lua
@@ -1,56 +1,66 @@
 return {
   -- 1. Add web-dev parsers to nvim-treesitter
-  -- This modifies your existing treesitter config
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
-        "javascript",
-        "typescript",
-        "tsx",
-        "css",
-        "html",
-        "json",
+        "javascript", "typescript", "tsx", "css", "html", "json",
       })
     end,
   },
+
   -- 2. Add web-dev tools to Mason
-  -- This modifies your existing mason.lua file's list
   {
-    "mason-org/mason.nvim", -- Updated repo name
+    "mason-org/mason.nvim",
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
-        -- LSPs
-        "typescript-language-server", -- Correct for TS/JS
-        "css-lsp", -- Fixed: was "cssls"
-        "html-lsp", -- Fixed: was "html"
+        -- CHANGE 1: Replaced "typescript-language-server" with "vtsls"
+        "vtsls",
+        "css-lsp",
+        "html-lsp",
         "eslint-lsp",
-        -- Formatter
         "prettierd",
       })
     end,
   },
+
   -- 3. Add web-dev language servers to nvim-lspconfig
-  -- This modifies your existing lspconfig setup
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      -- The 'servers' table will be merged with your existing one
-      -- (This will not touch your 'jdtls = false' setting)
       vim.tbl_deep_extend("force", opts.servers or {}, {
-        tsserver = {}, -- Still valid alias in lspconfig (maps to ts_ls)
-        cssls = {}, -- Still valid in lspconfig
-        html = {}, -- Still valid in lspconfig
+        -- CHANGE 2: Removed tsserver/ts_ls and added vtsls configuration
+        vtsls = {
+          -- This setting is crucial for JS/TS code actions to work reliably
+          settings = {
+            vtsls = {
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                completion = {
+                  enableServerSideFuzzyMatch = true,
+                },
+              },
+            },
+            typescript = {
+              updateImportsOnFileMove = { enabled = "always" },
+              inlayHints = {
+                parameterNames = { enabled = "literals" },
+                parameterTypes = { enabled = true },
+              },
+            },
+          },
+        },
+        cssls = {},
+        html = {},
         eslint = {},
       })
     end,
   },
-  -- 4. Add prettierd as the formatter for web files
-  -- This modifies your existing conform.nvim setup
+
+  -- 4. Add prettierd as the formatter
   {
     "stevearc/conform.nvim",
     opts = function(_, opts)
-      -- This will be merged with your existing python/black setup
       opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft or {}, {
         javascript = { "prettierd" },
         typescript = { "prettierd" },
